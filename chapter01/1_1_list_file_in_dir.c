bc1806d9ed7637a7b1746957a2aff8a5f0f0db4a
hewu2008 <hewu2008@gmail.com>
2014-12-15 14:23:33 +0800

    fix bug: 修复调用open_dir时缺少)的语法错误

--- chapter01/1_1_list_file_in_dir.c
+++ chapter01/1_1_list_file_in_dir.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 2) 
 		err_quit("usage: ls directory name");
-	if ((dp = opendir(argv[1]) == NULL) 
+	if ((dp = opendir(argv[1])) == NULL) 
 		err_sys("can't open %s", argv[1]);
 	while ((dirp = readdir(dp)) != NULL) 
 		printf("%s\n", dirp->name);
