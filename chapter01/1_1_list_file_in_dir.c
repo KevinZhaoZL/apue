e5c05fe24be4ebaff46d1c85ca8d3b09a080005d
hewu2008 <hewu2008@gmail.com>
2014-12-15 13:55:00 +0800

    列出一个目录下所有的文件

---
+++ chapter01/1_1_list_file_in_dir.c
@@ -0,0 +1,21 @@
+/**
+ * 列出一个目录下所有的文件
+ */
+#include "../include/apue.h"
+#include <dirent.h>
+
+int main(int argc, char *argv[]) 
+{
+	DIR *dp;
+	struct dirent *dirp;
+
+	if (argc != 2) 
+		err_quit("usage: ls directory name");
+	if ((dp = opendir(argv[1]) == NULL) 
+		err_sys("can't open %s", argv[1]);
+	while ((dirp = readdir(dp)) != NULL) 
+		printf("%s\n", dirp->name);
+
+	closedir(dp);
+	exit(0);
+}
