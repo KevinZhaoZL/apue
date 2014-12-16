e12429b85bb4442dcb6e42af6a2c31bba7067984
hewu2008 <hewu2008@gmail.com>
2014-12-16 14:38:30 +0800

    添加第三章文件io的源文件

---
+++ chapter03/3_5_set_file_status_flag.c
@@ -0,0 +1,18 @@
+/**
+ * 对一个文件描述符打开一个或多个文件状态标志
+ */
+#include "../include/apue.h"
+#include <fcntl.h>
+
+void set_fl(int fd, int flags) 
+{
+	int val;
+
+	if ((val = fcntl(fd, F_GETFL, 0)) < 0) 
+		err_sys("fcntl F_GETFL error");
+
+	val |= flags;
+
+	if (fcntl(fd, F_SETFL, val) < 0) 
+		err_sys("fcntl F_SETFL error");
+}
