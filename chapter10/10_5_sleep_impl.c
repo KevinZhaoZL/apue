27565aa31f1c0a16eb8010e3c1c45ba9052f793b
hewu2008 <hewu2008@gmail.com>
2015-01-26 11:03:00 +0800

    添加chapter11线程的源代码

---
+++ chapter10/10_5_sleep_impl.c
@@ -0,0 +1,14 @@
+#include <setjmp.h>
+#include <signal.h>
+#include <unistd.h>
+
+static jmp_buf env_alrm;
+
+static void sig_alrm(int signo)
+{
+	longjmp(env_alrm, 1);
+}
+
+unsigned int sleep2(unsigned int nsecs) {
+	
+}
