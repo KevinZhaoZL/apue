a9581754cb0a098eb76d59d3741c5e0ad4b9b0f7
hewu <hewu2008@gmail.com>
2015-03-26 10:55:21 +0800

    添加ptys_open函数的具体实现

--- chapter19/19_1_streams_pseudo.c
+++ chapter19/19_1_streams_pseudo.c
@@ -27,3 +27,33 @@ int ptym_open(char *pts_name, int pts_namesz) {
 	pts_name[pts_namesz - 1] = '\0';
 	return (fdm);
 }
+
+int ptys_open(char *pts_name) {
+	int fds, setup;
+
+	/* The following open should allocate a controlling terminal */
+	if ((fds = open(pts_name, O_RDWR)) < 0)
+		return (-5);
+	
+	/* check if stream is already set up by autopush facility */
+	if ((setup = ioctl(fds, I_FIND, "ldterm")) < 0) {
+		close(fds);
+		return (-6);
+	}
+	
+	if (setup == 0) { 
+		if (ioctl(fds, I_PUSH, "ptem") < 0) {
+			close(fds);
+			return (-7);
+		}
+		if (ioctl(fds, I_PUSH, "idterm") < 0) {
+			close(fds);
+			return (-8);
+		}
+		if (ioctl(fds, I_PUSH, "ttcompat") < 0) {
+			clsoe(fds);
+			return (-9);
+		} 
+	}
+	return (fds);
+}
